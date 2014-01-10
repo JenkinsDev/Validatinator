1b3bd5c02305208111e3f8a3dc48c98be90b0bdf
David Jenkins <david.nicholas.jenkins@gmail.com>
2014-01-09 19:48:56 -0500

    Added initial validation messages, these wil be the defaults, but they are overrideable.

--- js/validatinator.core.js
+++ js/validatinator.core.js
@@ -203,6 +203,28 @@ Validatinator.prototype = {
         // Validation exists, let's call it (Yes it's a bit of a weird method).
         return this["validations"][method].apply(this.validations, parameters);
     },
+    
+    validationMessages: {
+        accepted: "This field must be accepted.",
+        alpha: "This field only allows alpha characters.",
+        alphaDash: "This field only allows alpha, dash and underscore characters.",
+        alphaNum: "This field only allows alpha, dash, underscore and numerical characters.",
+        between: "This field must be between {min} and {max}.",
+        confirmed: "This field must be the same as {confirmedField}.",
+        contains: "This field must be one of the following values, {containsArray}.",
+        different: "This field must not be the same as {differentValue}.",
+        digitsLength: "This field must be a numerical values and {length} characters long.",
+        digitsLengthBetween: "This field must be between {minLength} and {maxLength}.",
+        email: "This field only allows valid email addresses.",
+        ipvFour: "This field only allows valid ipv4 addresses.",
+        max: "This field must only be {max} long or a numerical value must be less than or equal to {max}.",
+        min: "This field must be at least {min} long or a numerical value must be greater than or equal to {min}.",
+        notIn: "This field must not be contained within the following values, {notInArray}.",
+        number: "This field only allows valid numerical values.",
+        required: "This field is required.",
+        same: "This field must be the same value as {sameValue}.",
+        url: "This field only allows valid urls."
+    }
 };
 
 
