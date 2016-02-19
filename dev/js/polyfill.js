ec5c2863eed1315d94dfc7db2c8e57db39eff5d8
David Jenkins <david.nicholas.jenkins@gmail.com>
2016-02-18 21:40:26 -0500

    CommonJS support added.

--- dev/js/polyfill.js
+++ dev/js/polyfill.js
@@ -4,39 +4,41 @@
 */
 
 
-if (!String.prototype.contains) {
-  String.prototype.contains = function(str, startIndex) {
-    return -1 !== String.prototype.indexOf.call(this, str, startIndex);
-  };
-}
+module.exports = function() {
+  if (!String.prototype.contains) {
+    String.prototype.contains = function(str, startIndex) {
+      return -1 !== String.prototype.indexOf.call(this, str, startIndex);
+    };
+  }
 
-if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function(searchElement, fromIndex) {
-    if ( this === undefined || this === null ) {
-      throw new TypeError( '"this" is null or not defined' );
-    }
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(searchElement, fromIndex) {
+      if ( this === undefined || this === null ) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
 
-    var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+      var length = this.length >>> 0; // Hack to convert object.length to a UInt32
 
-    fromIndex = +fromIndex || 0;
+      fromIndex = +fromIndex || 0;
 
-    if (Math.abs(fromIndex) === Infinity) {
-      fromIndex = 0;
-    }
-
-    if (fromIndex < 0) {
-      fromIndex += length;
-      if (fromIndex < 0) {
+      if (Math.abs(fromIndex) === Infinity) {
         fromIndex = 0;
       }
-    }
 
-    for (;fromIndex < length; fromIndex++) {
-      if (this[fromIndex] === searchElement) {
-        return fromIndex;
+      if (fromIndex < 0) {
+        fromIndex += length;
+        if (fromIndex < 0) {
+          fromIndex = 0;
+        }
       }
-    }
 
-    return -1;
-  };
+      for (;fromIndex < length; fromIndex++) {
+        if (this[fromIndex] === searchElement) {
+          return fromIndex;
+        }
+      }
+
+      return -1;
+    };
+  }
 }
